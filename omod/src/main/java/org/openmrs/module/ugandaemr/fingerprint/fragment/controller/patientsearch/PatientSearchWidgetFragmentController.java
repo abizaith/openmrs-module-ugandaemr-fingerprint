d84d606a3a5cb16d1b1d1c4fcafae5c4328c168d
Simon Peter Muwanga <smuwanga@gmail.com>
2016-09-19 21:20:48 +0300

    controller and ui for fingerprint transferred from core

---
+++ omod/src/main/java/org/openmrs/module/ugandaemr/fingerprint/fragment/controller/patientsearch/PatientSearchWidgetFragmentController.java
@@ -0,0 +1,145 @@
+package org.openmrs.module.ugandaemr.fingerprint.fragment.controller.patientsearch;
+
+import org.apache.commons.lang.StringUtils;
+import org.openmrs.Patient;
+import org.openmrs.Person;
+import org.openmrs.PersonAttribute;
+import org.openmrs.PersonAttributeType;
+import org.openmrs.api.AdministrationService;
+import org.openmrs.api.PatientService;
+import org.openmrs.api.context.Context;
+import org.openmrs.module.appui.UiSessionContext;
+import org.openmrs.module.coreapps.CoreAppsConstants;
+import org.openmrs.module.emrapi.utils.GeneralUtils;
+import org.openmrs.ui.framework.SimpleObject;
+import org.openmrs.ui.framework.UiFrameworkConstants;
+import org.openmrs.ui.framework.UiUtils;
+import org.openmrs.ui.framework.annotation.FragmentParam;
+import org.openmrs.ui.framework.annotation.SpringBean;
+import org.openmrs.ui.framework.fragment.FragmentModel;
+import org.openmrs.ui.framework.fragment.action.FragmentActionResult;
+import org.openmrs.ui.framework.fragment.action.SuccessResult;
+import org.openmrs.util.OpenmrsConstants;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.text.SimpleDateFormat;
+import java.util.List;
+import java.util.Locale;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * Fragment controller for patient search widget; sets the min # of search characters based on global property,
+ * and loads last viewed patients for current user if "showLastViewedPatients" fragment config param=true
+ */
+public class PatientSearchWidgetFragmentController {
+
+    public void controller(FragmentModel model, UiSessionContext sessionContext,
+                           HttpServletRequest request,
+                           @SpringBean("adminService") AdministrationService administrationService,
+                           @FragmentParam(value = "showLastViewedPatients", required = false) Boolean showLastViewedPatients,
+                           @FragmentParam(value = "initialSearchFromParameter", required = false) String searchByParam) {
+
+        showLastViewedPatients = showLastViewedPatients != null ? showLastViewedPatients : false;
+
+        model.addAttribute("minSearchCharacters",
+                administrationService.getGlobalProperty(OpenmrsConstants.GLOBAL_PROPERTY_MIN_SEARCH_CHARACTERS, "1"));
+
+        model.addAttribute("searchDelayShort",
+                administrationService.getGlobalProperty(CoreAppsConstants.GP_SEARCH_DELAY_SHORT, "300"));
+
+        model.addAttribute("searchDelayLong",
+                administrationService.getGlobalProperty(CoreAppsConstants.GP_SEARCH_DELAY_LONG, "1000"));
+
+        model.addAttribute("dateFormatJS", "DD MMM YYYY");  // TODO really should be driven by global property, but currently we only have a property for the java date format
+        model.addAttribute("locale", Context.getLocale().getLanguage());
+        model.addAttribute("defaultLocale", new Locale(administrationService.getGlobalProperty((OpenmrsConstants.GLOBAL_PROPERTY_DEFAULT_LOCALE), "en")).getLanguage());
+        model.addAttribute("dateFormatter", new SimpleDateFormat(administrationService.getGlobalProperty(UiFrameworkConstants.GP_FORMATTER_DATE_FORMAT),
+                Context.getLocale()));
+        model.addAttribute("showLastViewedPatients", showLastViewedPatients);
+
+        String doInitialSearch = null;
+        if (searchByParam != null && StringUtils.isNotEmpty(request.getParameter(searchByParam))) {
+            doInitialSearch = request.getParameter(searchByParam);
+        }
+        model.addAttribute("doInitialSearch", doInitialSearch);
+
+        if (showLastViewedPatients) {
+            List<Patient> patients = GeneralUtils.getLastViewedPatients(sessionContext.getCurrentUser());
+            model.addAttribute("lastViewedPatients", patients);
+        }
+
+    }
+
+    public SimpleObject searchForPatientByFingerPrint(
+            @RequestParam(value = "datakey", required = false)String fingerPrintInBase64,
+            @SpringBean("patientService") PatientService service,
+            UiUtils ui){
+    	//String patientSearchPageUrl = "http://localhost:8080/openmrs/coreapps/findpatient/findPatient.page?app=patients.findPatientByFingerprint";
+    	String fingerPrintPersonAttributeTypeUUID = "0fe8824e-f9f8-42fa-a919-4d2dcd00a5da";
+    	Patient searchedPatient = new Patient();
+    	
+    	
+    	String uuid ="null";
+    	 System.out.println("Server reached");
+    	 
+    	
+    	try{
+    		Context.openSession();
+            Context.authenticate("admin", "Admin123");
+            
+
+        	//search patient attribute: leftIndexFingerPrint
+        	List<Patient> patients = Context.getPatientService().getAllPatients();
+    		System.out.println("Number of Patients: "+patients.size());
+        	
+        	if(fingerPrintInBase64!= null){
+        		
+        		
+        		for(Patient patientInstance : patients){
+        		//for(int index = lastIndex; index > -1; index--){
+        	    //		Patient patientInstance = patients.get(index);
+        			List<PersonAttribute> personAttributes = patientInstance.getActiveAttributes();
+        			
+        			for(PersonAttribute personAttribute: personAttributes){
+        				System.out.println("Person Attribute: "+personAttribute.getValue());
+        				
+        					System.out.println("Person Attribute Type UUID: "+personAttribute.getAttributeType().getUuid());
+        					if(personAttribute.getAttributeType().getUuid().equalsIgnoreCase(fingerPrintPersonAttributeTypeUUID)){
+            					System.out.println("Person attributeType passed...");
+            					//test if the base64 generated matches our stored base64 text
+            					if(personAttribute.getValue() != null ){
+            						System.out.println("Patient UUID: "+patientInstance.getUuid());
+            						uuid = patientInstance.getUuid();
+            						searchedPatient = patientInstance;
+            						break;
+            					}
+            				}
+        				
+        			}//end person attribute loop
+        
+        			if(uuid!="null"){
+        				break;
+        			}
+        		}//end patient loop
+        	}
+    	}catch(Exception e){
+    		System.out.println(".......exception..................................................................");
+    		e.getStackTrace();
+    	}
+    		finally{
+    	
+    		Context.closeSession();
+    	}
+    	
+    	String pageName = "patient.page?patientId="+uuid;
+        ui.pageLink("coreapps", pageName);
+    	String [] properties = {"uuid"};
+    	SimpleObject simplePatientObject = SimpleObject.fromObject(searchedPatient, ui, properties);
+    	
+    	
+    	return simplePatientObject;
+    }
+   
+}
---
+++ omod/src/main/webapp/fragments/patientsearch/patientSearchWidget.gsp
@@ -0,0 +1,105 @@
+<%
+    config.require("afterSelectedUrl")
+    def breadcrumbOverride = config.breadcrumbOverride ?: ""
+
+    ui.includeCss("uicommons", "datatables/dataTables_jui.css")
+    ui.includeCss("fingerprint", "patientsearch/patientSearchWidget.css")
+	ui.includeCss("fingerprint", "patientsearch/fontcustom_findpatient_fingerprint.css")
+    ui.includeJavascript("uicommons", "datatables/jquery.dataTables.min.js")
+    ui.includeJavascript("fingerprint", "patientsearch/patientSearchWidget.js")
+	ui.includeJavascript("fingerprint", "patientsearch/patientFingerPrintSearch.js")
+    ui.includeJavascript("uicommons", "moment-with-locales.min.js")
+  
+%>
+<script type="text/javascript">
+
+    var lastViewedPatients = [];
+    <%  if (showLastViewedPatients && !doInitialSearch) {
+            lastViewedPatients.each { it -> %>
+    lastViewedPatients.push({uuid:"${ it.uuid }",name:"${ it.personName ? ui.escapeJs(ui.format(it.personName)) : '' }",gender:"${ it.gender }",
+        age:"${ it.age ?: '' }", birthdate:"${ it.birthdate ? dateFormatter.format(it.birthdate) : '' }",
+        birthdateEstimated: ${ it.birthdateEstimated }, identifier:"${ it.patientIdentifier ? ui.escapeJs(it.patientIdentifier.identifier) : '' }"});
+    <%      }
+        }%>
+    function handlePatientRowSelection() {
+        this.handle = function (row) {
+            var uuid = row.uuid;
+            location.href = '/' + OPENMRS_CONTEXT_PATH + emr.applyContextModel('${ ui.escapeJs(config.afterSelectedUrl) }', { patientId: uuid, breadcrumbOverride: '${ ui.escapeJs(breadcrumbOverride) }'});
+        }
+    }
+    var handlePatientRowSelection =  new handlePatientRowSelection();
+    jq(function() {
+        var widgetConfig = {
+            initialPatients: lastViewedPatients,
+            doInitialSearch: ${ doInitialSearch ? "\"" + ui.escapeJs(doInitialSearch) + "\"" : "null" },
+            minSearchCharacters: ${ config.minSearchCharacters ?: 3 },
+            afterSelectedUrl: '${ ui.escapeJs(config.afterSelectedUrl) }',
+            breadcrumbOverride: '${ ui.escapeJs(breadcrumbOverride) }',
+            searchDelayShort: ${ searchDelayShort },
+            searchDelayLong: ${ searchDelayLong },
+            handleRowSelection: ${ config.rowSelectionHandler ?: "handlePatientRowSelection" },
+            dateFormat: '${ dateFormatJS }',
+            locale: '${ locale }',
+            defaultLocale: '${ defaultLocale }',
+            
+            messages: {
+                info: '${ ui.message("coreapps.search.info") }',
+                first: '${ ui.message("coreapps.search.first") }',
+                previous: '${ ui.message("coreapps.search.previous") }',
+                next: '${ ui.message("coreapps.search.next") }',
+                last: '${ ui.message("coreapps.search.last") }',
+                noMatchesFound: '${ ui.message("coreapps.search.noMatchesFound") }',
+                noData: '${ ui.message("coreapps.search.noData") }',
+                recent: '${ ui.message("coreapps.search.label.recent") }',
+                searchError: '${ ui.message("coreapps.search.error") }',
+                identifierColHeader: '${ ui.message("coreapps.search.identifier") }',
+                nameColHeader: '${ ui.message("coreapps.search.name") }',
+                genderColHeader: '${ ui.message("coreapps.gender") }',
+                ageColHeader: '${ ui.message("coreapps.age") }',
+                birthdateColHeader: '${ ui.message("coreapps.birthdate") }'
+            }
+        };
+
+        new PatientSearchWidget(widgetConfig);
+    });
+</script>
+<script type="text/javascript">
+    jq = jQuery;
+    
+    
+    
+    jq(function() {
+    jq('#testAjaxButton').click(function() {
+		        jq.getJSON('${ ui.actionLink("searchForPatientByFingerPrint") }',
+		            {
+		              'datakey': 'searchForPatientByFingerPrint'
+		              
+		             
+		            })
+		        .success(function(data) {
+		           
+		            window.location=".../../coreapps/clinicianfacing/patient.page?patientId=" + data.uuid;
+		            
+				 
+		        })
+		        .error(function(xhr, status, err) {
+		            alert('AJAX error ' + err);
+		        })
+		    });
+		});
+	
+</script>
+<form method="get" id="patient-search-form" onsubmit="return false">
+    <input type="text" id="patient-search" placeholder="${ ui.message("coreapps.findPatient.search.placeholder") }" autocomplete="off" <% if (doInitialSearch) { %>value="${doInitialSearch}"<% } %>/>
+    <i id="patient-search-clear-button" class="small icon-remove-sign"></i>
+    <i id="patient-search-finger-print-button" class="small icon-fingerprint"></i>
+</form>
+<div id="patient-search-finger-print" style="display:none;">
+    <br>
+	<button id="testAjaxButton" onclick="searchFingerPrint()"></button>
+    <p>Search functionality</p>
+	<applet code="org.openmrs.module.fingerprint.applet.PatientSearchApplet" archive="/openmrs/finger-print-applet.jar" width="650" height="300">
+            
+    </applet>
+</div>
+<div id="patient-search-results"></div>
